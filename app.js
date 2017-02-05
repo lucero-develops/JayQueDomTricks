cc17f2a5cdc913135e08403c4942106d2bc44217
lucero-develops <mike.j.lucero@gmail.com>
2017-02-04 21:18:41 -0500

    add to the DOM tree that already exists

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 /*
-For this quiz, use jQuery's val method to make live changes to the 'Cool Articles' <h1>!
+For this quiz, you'll need to add to the DOM tree that already exists.
 
-The starter code below creates an event listener that will run any time the input changes.
-For more on events, check the instructor notes.
+'#family2' should be a sibling of and come after '#family1'. '#bruce' should be the only immediate child
+of '#family2'. '#bruce' should have two <div>s as children, '#madison' and '#hunter'.
 */
+// Your code goes here!
+ 
+var family1, family2, bruce, madison, hunter;
+
+family1 = $('#family1');
+
+family2 = $('<div id="family2"><h1>Family 2</h1></div>');
+
+bruce = $('<div id ="bruce"><h2>Bruce</h2></div>');
+
+madison = $('<div id ="madison"><h3>Madison</h3></div>');
+
+hunter = $('<div id ="hunter"><h3>Hunter</h3></div>');
+
+family2.insertAfter(family1);
+
+family2.append(bruce);
+
+bruce.append(madison);
+
+bruce.append(hunter);
 
 
-$('#input').on('change', function(){
-	var val, h1;
 
-	val = $('#input').val();
 
-	h1 = $('.articles').children('h1');
 
-	h1.text(val);
-});
