73d3896ae05f343ad9f21a11bdd861860c0af830
lucero-develops <mike.j.lucero@gmail.com>
2017-02-04 20:41:22 -0500

    val method to make live changes

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 /*
-For this quiz, set the href of the <a> in the first nav item to "#1".
+For this quiz, use jQuery's val method to make live changes to the 'Cool Articles' <h1>!
 
-You must use jQuery's attr() method!
+The starter code below creates an event listener that will run any time the input changes.
+For more on events, check the instructor notes.
 */
 
-var navList;
-var firstItem;
-var link;
 
-navList = $('.nav-list');
+$('#input').on('change', function(){
+	var val, h1;
 
-firstItem = navList.childern().first();
+	val = $('#input').val();
 
-link = firstItem.find('a');
+	h1 = $('.articles').children('h1');
 
-link.attr('href', '#1');
-
+	h1.text(val);
+});
