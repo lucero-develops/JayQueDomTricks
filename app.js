422cab496b6853a9bc2880a1244c0f016e3a6429
lucero-develops <mike.j.lucero@gmail.com>
2017-02-04 22:40:10 -0500

    listened for the click event on the my-button button

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
 /*
-
-For this quiz, can you use this script, which is linked in the <head> of index.html,
-to change the boring placeholder image to a picture of a cute animal?
-
-Remember, you'll need to pass a function into the jQuery object to run
-when the document is ready.
-
-Unfortunately, placepuppy is no longer available. Here's a link to a random
-animal image on lorempixel.com:
-http://lorempixel.com/350/150/animals/
-
-Good luck!
-
+For this quiz, use jQuery to set up an event listener. Your event listener must:
+    1. listen to the #my-button element
+    2. listen for a `click` event
+    3. perform the following actions when the button is clicked:
+        a. remove the #my-button element from the DOM
+        b. add the `success` class to the body
 */
 
-$(function() {
-  $('img').attr('src', 'http://lorempixel.com/350/150/animals');
-})
+$('#my-button').on('click', function(){
+	$(this).remove();
+	$('body').addClass('success');
+});
 
-/*
-I'm simply starting with the jQuery object and passing it an anonymous function.
 
-The anonymous function changes the src of the one img on the page to the URL provided.
-(Remember, $('img') grabs all of the images on the page, so this is a very bad selector. 
-It works in this case because there's only one <img>, but normally you should use a much more
- specific selector.)
-
-If I hadn't wrapped my .attr() function in the jQuery object, it would run as soon as it's loaded
-in the <head> of the document, which occurs before the <img> tag appears on the page. So nothing 
-would happen.
-
-But by wrapping it up in the jQuery object, it runs when the DOM is ready and
-I get to see a cute puppy instead!
